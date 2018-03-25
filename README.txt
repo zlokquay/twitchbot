fad9ba4423a8b547c7fe8ca321f67cba69fae3af
Zachary Taylor <35826908+zlokquay@users.noreply.github.com>
2018-03-25 13:00:48 -0400

    Initialized Repo with bot

---
+++ README.txt
@@ -0,0 +1,10 @@
+!README! (V1.0.0)
+--------
+OoTBingoBot is a simple bot to allow the user to set a bingo board and users to see the url for that bingo board.
+--------
+Commands:
+!board - shows the current board the streamer is using
+
+!setboard (url) - allows the streamer to set the url for the board. 
+  parameter (url) - the url of the board.
+  !setboard only works for those with whitelisted names. To set a name as a whitelisted name, you   have to open the app.js file with a text editor of your choice (notepad++, atom, SUMBLIME) and add   the names to the "channels" array in the options variable.
---
+++ app.js
@@ -0,0 +1,50 @@
+//written for Fenyan by Zlokquay
+//Can be expanded upon.
+
+var tmi = require('tmi.js');
+var board = "empty";
+var tempBoard;
+var username;
+
+var options = {
+	options: {
+		debug: true
+	},
+	connection: {
+		cluster: "aws",
+		reconnect: true
+	},
+	identity:{
+		username: "OoTBingoBot",
+		password: "oauth:hqeiowqb7dy6mpfaodic0gygz7dvpn"
+	},
+	channels:["Fenyan"]
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on("chat", function(channel, user, message, self){
+	if(message === "!board") {
+		client.action("Fenyan", "The current board is: " + board);
+	}
+});
+
+client.on("chat", function(channel, user, message, self){
+	tempBoard = message.split(" ", 2);
+	username = user.username;
+	if((tempBoard[0] === "!setboard" && username.toLowerCase() === "fenyan") || (tempBoard[0] === "!setboard" && username.toLowerCase() === "zlokquay")){
+		board = tempBoard[1]
+		client.action("Fenyan", "Board set!");
+	}
+});
+
+client.on("chat", function(channel, user, message, self){
+	if(message === "!clearboard"){
+		board = "No Current Board.";
+	}
+});
+
+client.on("connected", function(address, port){
+	client.action("Fenyan", "Hello, I'm your bot!");
+});
